ecdd06d5136a2bd5b2b1a1687e8fd6f7570782c0
Bernardo Euler <bernardoeulersantos@gmail.com>
2023-10-11 23:25:28 -0300

    Denunciator details screen

--- src/screens/DenunciatorDetails.js
+++ src/screens/DenunciatorDetails.js
@@ -1,65 +1,33 @@
 import React, { useState, useEffect } from "react"
-import { ScrollView, StatusBar, Pressable, Heading, Select, TextArea, VStack, Button, Modal, Text, CloseIcon, Box, Icon, Center, FlatList, IconButton } from "native-base"
-import { Alert } from "react-native"
+import { ScrollView, StatusBar, Pressable, Heading, Image, TextArea, VStack, Button, Modal, Text, CloseIcon, Box, Icon, Center } from "native-base"
 import { useNavigation } from "@react-navigation/native"
-import { MaterialIcons } from "@expo/vector-icons"
-import Loading from "../components/Loading"
 import styles from "../styles/global"
 import theme from "../config/theme"
-import { auth, firestore } from "../config/firebase"
-import getSpecificDoc from "../utils/getSpecificDoc"
-import storeData from "../utils/storeData"
-import { addDoc, collection } from "firebase/firestore"
 
 function DenunciatorDetails() {
   const { colors } = theme
   const navigation = useNavigation()
-  const [isBtnLoading, setIsBtnLoading] = useState(false)
 
-  const [description, setDescription] = useState("")
+  function handleSubmit(type) {
+    console.log("Submiting details...", type)
 
-  async function handleSubmit() {
-    const authenticatedUserId = auth.currentUser.uid
-    const usersRef = collection(firestore, "users")
-    const denunciationData = {
-      local,
-      hour,
-      date,
-      description,
-      status: "pending",
-      timestamp: Date.now()
-    }
-
-    console.log("Submiting denunciation...")
-
-    setIsBtnLoading(true)
-
-    try {
-      const userDoc = await getSpecificDoc(usersRef, "userId", authenticatedUserId)
-      await storeData(denunciationData, `users/${userDoc.documentId}/denunciations`)
-      console.log("Data stored in database")
-      setIsBtnLoading(false)
-      Alert.alert("Denúncia enviada com sucesso!", "Abra a aba Minha denúncias para ver a denúncia que acabou de fazer.", [{text: "OK", onPress: () => navigation.goBack()}])
-    }
-
-    catch (err) {
-      setIsBtnLoading(false)
-      Alert.alert("Não foi possível enviar sua denúncia", "Houve um erro inesperado, verifique sua conexão e tente novamente.")
-      console.warn("Error: " + err)
-    }
+    navigation.navigate("NewDenunciation", { denunciatorType: type })
   }
 
   return (
     <ScrollView style={{...styles.Container}}>
       <StatusBar barStyle="light-content" backgroundColor={colors.primary[500]} />
 
-      <Heading mt={4} size="h3" color="neutral.700">Quem é você?</Heading>
+      <Heading mt={4} px={4} size="h5" color="neutral.700" textAlign="center">Qual o seu envolvimento na situação?</Heading>
 
-      <VStack w="100%" space={4} mt={6}>
-
-      </VStack>
-
-      <Button isLoading={isBtnLoading} onPress={() => navigation.navigate("NewDenunciation")} mt={8} mb={6}>Finalizar</Button>
+      <Pressable mt={8} mb={6} p={4} borderColor="neutral.100" borderWidth="1" borderRadius="24" alignItems="center" onPress={() => handleSubmit("victim")}>
+        <Image source={require("../assets/images/profilepic.png")} alt="victim" size={40} />
+        <Text size="large" color="neutral.600" fontWeight="semibold">Vítima</Text>
+      </Pressable>
+      <Pressable  mt={8} mb={6} p={4} borderColor="neutral.100" borderWidth="1" borderRadius="24" alignItems="center" onPress={() => handleSubmit("communicant")}>
+        <Image source={require("../assets/images/speaking.png")} alt="communicant" size={40} />
+        <Text size="large" color="neutral.600" fontWeight="semibold">Comunicante</Text>
+      </Pressable>
     </ScrollView>
   )
 }
