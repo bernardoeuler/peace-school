b1cc5f717bcc73869a389a910f5333c69cbed252
Bernardo Euler <bernardoeulersantos@gmail.com>
2023-10-14 18:32:22 -0300

    Change feedback message when the list is empty

--- src/components/EmptyDenunciationsFeedback.js
+++ src/components/EmptyDenunciationsFeedback.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Center, Text } from "native-base"
 
 function EmptyDenunciationsFeedback({ activeFilter }) {
-  const statusText = activeFilter === "pending" ? "em andamento" : "resolvidas"
+  const statusText = activeFilter === "pending" ? "Em análise" : "já atendidas"
 
   return (
     <Center flex={1} mt={4}>
