b59cde7d2d42f79839fc55f5ce18c50147d87f1e
Bernardo Euler <bernardoeulersantos@gmail.com>
2023-10-14 21:33:17 -0300

    Implement denunciation details

--- src/screens/DenunciationDetails.js
+++ src/screens/DenunciationDetails.js
@@ -1,22 +1,43 @@
-import {
-  Text,
-  Image as NBImage,
-  Button as NBButton,
-  Box,
-  Heading,
-  Input,
-} from "native-base"
+import { ScrollView, VStack, Heading, Text, FlatList, Center, Box } from "native-base"
 
-import { SafeAreaView } from "react-native"
+import theme from "../config/theme"
 
 import styles from "../styles/global"
 
-function DenunciationDetails({ navigation, route }) {
-  const denunciationId = route.params.id
+function DenunciationDetails({ route }) {
+  const { colors } = theme
+  const { documentId: denunciationId, status, hour, date, local, description } = route.params
+  const statusText = status === "pending" ? "Em análise" : "Atendida"
+
   return (
-    <SafeAreaView style={{...styles.Container, justifyContent: "space-between"}}>
-      <Text>Detalhes de uma denúncia {denunciationId}</Text>
-    </SafeAreaView>
+    <ScrollView style={{...styles.Container}} showsVerticalScrollIndicator={false}>
+      <VStack space={8} >
+        <VStack space={1}>
+          <Heading size="h6">Status da denúncia</Heading>
+          <Text color="neutral.600" fontWeight="medium">{statusText}</Text>
+        </VStack>
+
+        <VStack space={1}>
+          <Heading size="h6">Horário</Heading>
+          <Text color="neutral.600" fontWeight="medium">{hour}</Text>
+        </VStack>
+
+        <VStack space={1}>
+          <Heading size="h6">Data</Heading>
+          <Text color="neutral.600" fontWeight="medium">{date}</Text>
+        </VStack>
+        
+        <VStack space={1}>
+          <Heading size="h6">Local</Heading>
+          <Text color="neutral.600" fontWeight="medium">{local}</Text>
+        </VStack>
+
+        <VStack space={1}>
+          <Heading size="h6">Descrição</Heading>
+          <Text color="neutral.600" fontWeight="medium">{description}</Text>
+        </VStack>
+      </VStack>
+    </ScrollView>
   )
 }
 
