e19a3f4d3b56596b38892a739e6995d6c9df4f57
Bernardo Euler <bernardoeulersantos@gmail.com>
2023-10-15 14:54:26 -0300

    Add elements to view profile screen

--- src/screens/ViewProfile.js
+++ src/screens/ViewProfile.js
@@ -1,21 +1,67 @@
-import {
-  Text,
-  Image as NBImage,
-  Button as NBButton,
-  Box,
-  Heading,
-  Input,
-} from "native-base"
-
+import React, { useState, useEffect } from "react"
+import { Box, HStack, Heading, Pressable, Text, VStack } from "native-base"
 import { SafeAreaView } from "react-native"
-
+import { FontAwesome5 } from "@expo/vector-icons"
+import { Octicons } from "@expo/vector-icons"
+import theme from "../config/theme"
 import styles from "../styles/global"
+import { collection } from "firebase/firestore"
+import { auth, firestore } from "../config/firebase"
+import getSpecificDoc from "../utils/getSpecificDoc.js"
 
 function ViewProfile({ navigation }) {
+  const { colors } = theme
+  const [userName, setUserName] = useState("")
+
+  useEffect(() => {
+    (async () => {
+      const userDoc = await getUserData()
+      const firstUserName = userDoc.name.split(" ")[0]
+      setUserName(firstUserName)
+    })()
+  }, [])
+
+  async function getUserData() {
+    const authenticatedUserId = auth.currentUser.uid
+    const usersRef = collection(firestore, "users")
+    return await getSpecificDoc(usersRef, "userId", authenticatedUserId)
+  }
+
   return (
-    <SafeAreaView style={{...styles.Container, justifyContent: "space-between"}}>
-      <Text>Ver perfil</Text>
-      <NBButton onPress={() => navigation.navigate("EditProfile")}>Editar perfil</NBButton>
+    <SafeAreaView style={{...styles.Container, paddingHorizontal: 0, justifyContent: "space-between", alignItems: "center"}}>
+      <VStack alignItems="center" space={4}>
+        <Box w={40} h={40} borderRadius="full" alignItems="center" justifyContent="center" bgColor="neutral.50" overflow="hidden">
+          <FontAwesome5 name="user" size={80} color={colors.neutral[100]} />
+        </Box>
+        <Heading size="h6" textAlign="center">{userName}</Heading>
+      </VStack>
+
+      <VStack w="100%" space={0}>
+        <Pressable _pressed={{backgroundColor: "neutral.50"}}>
+          <HStack alignItems="center" px={10} py={6} space={6}>
+            <Octicons name="person" size={32} color={colors.neutral[500]} />
+            <Text size="large" fontWeight="semibold" color="neutral.500">Detalhes da conta</Text>
+          </HStack>
+        </Pressable>
+        <Pressable _pressed={{backgroundColor: "neutral.50"}}>
+          <HStack alignItems="center" px={10} py={6} space={5}>
+            <Octicons name="gear" size={32} color={colors.neutral[500]} />
+            <Text size="large" fontWeight="semibold" color="neutral.500">Configurações</Text>
+          </HStack>
+        </Pressable>
+        <Pressable _pressed={{backgroundColor: "neutral.50"}}>
+          <HStack alignItems="center" px={10} py={6} space={5}>
+            <Octicons name="people" size={32} color={colors.neutral[500]} />
+            <Text size="large" fontWeight="semibold" color="neutral.500">Suporte</Text>
+          </HStack>
+        </Pressable>
+        <Pressable _pressed={{backgroundColor: "neutral.50"}} onPress={() => {auth.signOut()}}>
+          <HStack alignItems="center" px={10} py={6} space={7}>
+            <Octicons name="sign-out" size={32} color={colors.neutral[500]} />
+            <Text size="large" fontWeight="semibold" color="neutral.500">Sair</Text>
+          </HStack>
+        </Pressable>
+      </VStack>
     </SafeAreaView>
   )
 }
