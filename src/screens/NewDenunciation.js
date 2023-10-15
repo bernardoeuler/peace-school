edcbabc2ea2f68ae485001ea1e114a5c913443ca
Bernardo Euler <bernardoeulersantos@gmail.com>
2023-10-15 19:23:52 -0300

    Create date and time pickers in new denunciation screen

--- src/screens/NewDenunciation.js
+++ src/screens/NewDenunciation.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react"
-import { ScrollView, StatusBar, Image, Select, TextArea, VStack, Button, Modal, Text, CloseIcon, Box, Icon, Center, FlatList, IconButton, Input } from "native-base"
+import { ScrollView, StatusBar, Select, TextArea, VStack, Button, Text, Input, Pressable } from "native-base"
 import { Alert } from "react-native"
+import DateTimePicker from "@react-native-community/datetimepicker"
 import { useNavigation } from "@react-navigation/native"
 import styles from "../styles/global"
 import theme from "../config/theme"
+import { collection } from "firebase/firestore"
 import { auth, firestore } from "../config/firebase"
 import getSpecificDoc from "../utils/getSpecificDoc"
+import parseTimestampToHours from "../utils/parseTimestampToHours"
+import parseTimestampToDate from "../utils/parseTimestampToDate.js"
 import storeData from "../utils/storeData"
-import { collection } from "firebase/firestore"
 
 function NewDenunciation({ route }) {
   const { denunciatorType } = route.params
@@ -19,6 +22,25 @@ function NewDenunciation({ route }) {
   const [date, setDate] = useState("")
   const [local, setLocal] = useState("")
   const [description, setDescription] = useState("")
+  
+  const [showTimePicker, setShowTimePicker] = useState(false)
+  const [showDatePicker, setShowDatePicker] = useState(false)
+
+  function onTimeChange(event, newTimestamp) {
+    setShowTimePicker(false)
+    if (event.type === "set") {
+      const newTime = parseTimestampToHours(newTimestamp)
+      setHour(newTime)
+    }
+  }
+
+  function onDateChange(event, newTimestamp) {
+    setShowDatePicker(false)
+    if (event.type === "set") {
+      const newDate = parseTimestampToDate(newTimestamp)
+      setDate(newDate)
+    }
+  }
 
   async function handleSubmit() {
     const authenticatedUserId = auth.currentUser.uid
@@ -58,10 +80,14 @@ function NewDenunciation({ route }) {
 
       <VStack w="100%" space={4}>
         <Text fontWeight="bold" color="neutral.700">Horário</Text>
-        <Input onChangeText={setHour} placeholder="00:00" />
+        <Pressable onPress={() => setShowTimePicker(true)}>
+          <Input isReadOnly value={hour} placeholder={parseTimestampToHours(Date.now())} />
+        </Pressable>
 
         <Text mt={4} fontWeight="bold" color="neutral.700">Data</Text>
-        <Input onChangeText={setDate} placeholder="dd/mm/aaaa" />
+        <Pressable onPress={() => setShowDatePicker(true)}>
+          <Input isReadOnly value={date} placeholder={parseTimestampToDate(Date.now())} />
+        </Pressable>
 
         <Text mt={4} fontWeight="bold" color="neutral.700">Local</Text>
         <Input onChangeText={setLocal} placeholder="Ex: Quadra de futebol, cantina, etc." />
@@ -69,6 +95,27 @@ function NewDenunciation({ route }) {
         <Text mt={4} fontWeight="bold" color="neutral.700">Descrição</Text>
         <TextArea onChangeText={setDescription} h="240px" placeholder="Descreva como ocorreu a situação e dê informações adicionais, como a aula em que aconteceu e a sala dos envolvidos." fontSize={16} _focus={{bg: "neutral.50"}}/>
       </VStack>
+
+      {showTimePicker && 
+      <DateTimePicker
+        value={new Date()}
+        mode="time" 
+        onChange={onTimeChange}
+        negativeButton={{label: "Cancelar"}}
+        positiveButton={{label: "OK"}}
+        is24Hour
+      />}
+
+      {showDatePicker && 
+      <DateTimePicker
+        value={new Date()}
+        mode="date" 
+        onChange={onDateChange}
+        minimumDate={new Date(2023, 0, 1)}
+        maximumDate={new Date()}
+        negativeButton={{label: "Cancelar"}}
+        positiveButton={{label: "OK"}}
+      />}
 
       <Button isLoading={isBtnLoading} onPress={handleSubmit} mt={8} mb={16}>Finalizar</Button>
     </ScrollView>
