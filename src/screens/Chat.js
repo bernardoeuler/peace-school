9e3f130017658b556f2bdf30d7970c7adb958eb6
Bernardo Euler <bernardoeulersantos@gmail.com>
2023-10-15 13:05:07 -0300

    Add psychologist first message and input placeholder

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,5 +1,5 @@
+import { ScrollView, HStack, IconButton, TextArea, Box } from "native-base"
 import { SafeAreaView } from "react-native"
-import { ScrollView, HStack, IconButton, TextArea } from "native-base"
 import { Ionicons } from "@expo/vector-icons"
 import styles from "../styles/global"
 import theme from "../config/theme"
@@ -9,9 +9,11 @@ function Chat() {
 
   return (
     <SafeAreaView style={{...styles.Container}}>
-      <ScrollView></ScrollView>
+      <ScrollView>
+        <Box display="inline" maxW="80%" p={4} borderRadius={8} bgColor="neutral.50">Olá! Eu irei te atender, como posso ajudar?</Box>
+      </ScrollView>
       <HStack space={2} >
-        <TextArea flex={1} px={4} h={12} totalLines={2} />
+        <TextArea flex={1} px={4} h={12} totalLines={2} placeholder="Digite aqui" />
         <IconButton size={12} borderRadius="full" bgColor="primary.500" icon={<Ionicons name="send" size={24} color="white" />} />
       </HStack>
     </SafeAreaView>
