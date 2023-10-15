2f4dbdb95fec9272c2bf26f6b0891384cb4d431a
Bernardo Euler <bernardoeulersantos@gmail.com>
2023-10-14 23:15:08 -0300

    Change fixed name to user's name

--- src/screens/ChatIntroduction.js
+++ src/screens/ChatIntroduction.js
@@ -1,20 +1,31 @@
-import {
-  Text,
-  Image as NBImage,
-  Button as NBButton,
-  Box,
-  Heading,
-} from "native-base"
-
+import React, { useState, useEffect } from "react"
+import {Text, Image as NBImage, Button as NBButton, Box, Heading, } from "native-base"
 import { SafeAreaView } from "react-native"
-
 import styles from "../styles/global"
+import { collection } from "firebase/firestore"
+import { auth, firestore } from "../config/firebase"
+import getSpecificDoc from "../utils/getSpecificDoc.js"
 
 function ChatIntroduction({ navigation }) {
+  const [userName, setUserName] = useState("")
+
+  useEffect(() => {
+    (async () => {
+      const userDoc = await getUserData()
+      setUserName(userDoc.name)
+    })()
+  }, [])
+
+  async function getUserData() {
+    const authenticatedUserId = auth.currentUser.uid
+    const usersRef = collection(firestore, "users")
+    return await getSpecificDoc(usersRef, "userId", authenticatedUserId)
+  }
+
   return (
     <SafeAreaView style={{...styles.Container, justifyContent: "space-between"}}>
       <Box>
-        <Heading size="h4">Olá, Bernardo</Heading>
+        <Heading size="h4">Olá, {userName}</Heading>
         <Text mt={2} size="large">Que tal conversar com alguém para se sentir melhor? Fale com um de nosso psicólogos e eles vão poder te ajudar.</Text>
       </Box>
       <Box style={{alignItems: "center"}}>
