ea81740658842590d7ec19105cdebcdc4e8b7209
Bernardo Euler <bernardoeulersantos@gmail.com>
2023-10-16 14:07:07 -0300

    Add icons to the bottom tab bar

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -4,27 +4,68 @@ import HomeRoutes from "../routes/home.routes.js"
 import ChatRoutes from "../routes/chat.routes.js"
 import NewDenunciationRoutes from "../routes/newDenunciation.routes.js"
 import DenunciationsListRoutes from "../routes/denunciations.routes.js"
-import Profile from "../routes/profile.routes.js"
+import ProfileRoutes from "../routes/profile.routes.js"
+import { AntDesign } from "@expo/vector-icons"
 import hideTab from "../utils/hideTab.js"
+import theme from "../config/theme"
 
 function AppRoutes() {
   const Tab = createBottomTabNavigator()
+  const { colors } = theme
 
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}> 
+    <Tab.Navigator 
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colors.primary[500],
+        tabBarInactiveTintColor: colors.neutral[500]
+      }}
+    > 
       <Tab.Screen
         name="HomeRoutes"
         component={HomeRoutes}
-        options={({ route }) => ({...hideTab(route, "Article"), title: "Início"})}
+        options={({ route }) => ({
+          title: "Início",
+          ...hideTab(route, "Article"),
+          tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />,
+        })}
       />
       <Tab.Screen
         name="ChatRoutes"
         component={ChatRoutes}
-        options={({ route }) => ({...hideTab(route, "Chat"), title: "Chat"})}
+        options={({ route }) => ({
+          title: "Chat",
+          ...hideTab(route, "Chat"),
+          tabBarIcon: ({ color }) => <AntDesign name="message1" size={24} color={color} />
+        })}
       />
-      <Tab.Screen name="NewDenunciationRoutes" component={NewDenunciationRoutes} options={{title: "Nova denúncia", tabBarStyle: {display: "none"}}} />
-      <Tab.Screen name="DenunciationsListRoutes" component={DenunciationsListRoutes} options={({ route }) => ({...hideTab(route, "DenunciationDetails"), title: "Denúncias"})} />
-      <Tab.Screen name="ProfileRoutes" component={Profile} options={({ route }) => ({...hideTab(route, "EditProfile"), title: "Perfil"})} />
+      <Tab.Screen
+        name="NewDenunciationRoutes"
+        component={NewDenunciationRoutes}
+        options={{
+          title: "Nova denúncia",
+          tabBarStyle: {display: "none"},
+          tabBarIcon: ({ color }) => <AntDesign name="pluscircleo" size={24} color={color} />
+        }}
+      />
+      <Tab.Screen
+        name="DenunciationsListRoutes"
+        component={DenunciationsListRoutes}
+        options={({ route }) => ({
+          title: "Denúncias",
+          ...hideTab(route, "DenunciationDetails"),
+          tabBarIcon: ({ color }) => <AntDesign name="bars" size={24} color={color} />
+        })} 
+      />
+      <Tab.Screen
+        name="ProfileRoutes"
+        component={ProfileRoutes}
+        options={({ route }) => ({
+          title: "Perfil",
+          ...hideTab(route, "EditProfile"),
+          tabBarIcon: ({ color }) => <AntDesign name="user" size={24} color={color} />
+        })}
+      />
     </Tab.Navigator>
   )
 }
